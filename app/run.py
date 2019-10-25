1797d10114006019e4ad6a272212ef2adcbcfc92
Jujhar Singh <jujhar@jujhar.com>
2019-10-25 12:04:59 +0100

    basic code in place, ready to test

--- app/run.py
+++ app/run.py
@@ -27,17 +27,21 @@ print(f"Waiting for falling edge on port {PIN_BUTTON_A} or {PIN_BUTTON_B}")
 # During this waiting time, your computer is not
 # wasting resources by polling for a button press
 
+GPIO.add_event_detect(PIN_BUTTON_A, GPIO.RISING)
+GPIO.add_event_detect(PIN_BUTTON_B, GPIO.RISING)
+
 try:
     while True:
-        # interrupt, wait until true
-        (GPIO.wait_for_edge(PIN_BUTTON_B, GPIO.RISING)
-         or GPIO.wait_for_edge(PIN_BUTTON_A, GPIO.RISING))
-        if GPIO.input(PIN_BUTTON_A):
+        if GPIO.event_detected(PIN_BUTTON_A):
             print(f"\n Button pressed {PIN_BUTTON_A}")
             GPIO.output(PIN_LED_A, GPIO.HIGH)
             time.sleep(1)
             GPIO.output(PIN_LED_A, GPIO.LOW)
-
+        if GPIO.event_detected(PIN_BUTTON_B):
+            print(f"\n Button pressed {PIN_BUTTON_B}")
+            GPIO.output(PIN_LED_B, GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(PIN_LED_B, GPIO.LOW)
 except KeyboardInterrupt:
     GPIO.cleanup()       # clean up GPIO on CTRL+C exit
 
